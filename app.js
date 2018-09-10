870d12f2ee992a5e0104a74c5c2fb688a8290c10
JohnMichelin <jmichelinphone@gmail.com>
2018-09-10 09:34:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ $(document).ready(function() {
 
   $(".add-text-btn").on("click", function(){
 
-    // store values
+    // using jquery selector to read input values
     let inputKey = $(".user-input-title").val();
     let inputValue = $(".user-input-body").val();
 
-    // clear values
+    // clear values on the display
     $(".user-input-title").val("");
     $(".user-input-body").val("");
-
+    
+    // console log the input values {key:value}
     console.log(inputKey, inputValue);
-
+    
+    
+    //https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
     localStorage.setItem(inputKey, inputValue);
-    // data-
+    
+    // data-uniqID
     let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' ' +  localStorage.getItem(inputKey) + '</div>';
     $(".display").html(itemHtml);
     //console.log(localStorage);
@@ -24,7 +28,7 @@ $(document).ready(function() {
     $(".display-item").on("click", function(e){
       // plop the key:value back into the input boxes
 
-      // get the values from the the divs?
+      // get the values from the the data dash properties
       console.log("key=> ", e.target.dataset.storageKey); // user-input-title
       localStorage.getItem(e.target.dataset.storageKey); // user-input-body
 
@@ -38,11 +42,12 @@ $(document).ready(function() {
 
 
    // TODO add back in later
-   // $(".user-input").on("keyup", function(){
-   //   let inputValue = $(".user-input").val();
-   //   localStorage.setItem("testStorage", inputValue);
-   //   $(".display").text(localStorage.getItem("testStorage"));
-   // });
+  // example of how to do a filter based on a keyup event
+//    $(".user-input").on("keyup", function(){
+//      let inputValue = $(".user-input").val();
+//      localStorage.setItem("testStorage", inputValue);
+//      $(".display").text(localStorage.getItem("testStorage"));
+//    });
 
    $(".del-text-btn").on("click", function() {
      alert('item deleted? check the console'); // maybe change to a window.confirm
